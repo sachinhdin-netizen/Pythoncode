c9f665b90fdc722e6886cee4a658ddddad19850b
sachinhdin-netizen <sachinhd.in@gmail.com>
2026-03-20 16:41:55 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,9 @@
 import unittest
 from main import add
+
 class TestAdd(unittest.TestCase):
-def test_add(self):
-self.assertEqual(add(5, 5), 10)
-if name == 'main':
-unittest.main()
+    def test_add(self):
+        self.assertEqual(add(5, 5), 10)
+
+if __name__ == "__main__":
+    unittest.main()
