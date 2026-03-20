ec9bbace88bd91e5c54d40488d1d86534b3b6d10
sachinhdin-netizen <sachinhd.in@gmail.com>
2026-03-20 17:20:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ def add(a, b):
     return a + b
 
 if __name__ == "__main__":
+    import sys
+
     print("Running main.py...")
     print("Sum:", add(10, 10))
+
+    # Force flush so Jenkins shows output immediately
+    sys.stdout.flush()
