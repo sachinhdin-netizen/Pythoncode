fd131c56a00243c11987b1ac1ebac8a5b4cbea9b
sachinhdin-netizen <sachinhd.in@gmail.com>
2026-03-20 18:16:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"<h1>Hello World Team</h1>")
+        self.wfile.write(b"<h1>Hello World</h1>")
 
 def run():
     server_address = ('0.0.0.0', 9090)  # Listen on all interfaces
