b98a38744aa424578ea90ab8ed57cd05c78f2dde
sachinhdin-netizen <sachinhd.in@gmail.com>
2026-03-20 17:54:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.wfile.write(b"<h1>Hello World</h1>")
 
 def run():
-    server_address = ('0.0.0.0', 9090)
+    server_address = ('0.0.0.0', 9090)  # Listen on all interfaces
     httpd = HTTPServer(server_address, HelloHandler)
     print("Serving Hello World on port 9090...")
     httpd.serve_forever()
